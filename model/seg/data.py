5a2d07d7ac0f094d8429c63a1a417c68c6226730
johnathanchiu <johnathanchiu@berkeley.edu>
2024-09-30 02:28:25 -0400

    building out dataloader properly for training

--- model/seg/data.py
+++ model/seg/data.py
@@ -40,33 +40,6 @@ class DocumentSegmentationDataset(Dataset):
 
         return strips, labels  # Return the strips and their corresponding labels
 
-    def create_strips_and_labels(self, image, img_name):
-        # Convert image to numpy array for processing
-        image_array = np.array(image)
-        height, width = image_array.shape[:2]
-        strip_height = 32  # Define the height of each strip
-        strips = []
-        labels = []
-
-        # Get bounding boxes for the current image
-        bboxes = self.bbox_data.get(
-            os.path.basename(img_name), []
-        )  # Get bounding boxes for the image
-
-        # Create strips from the image
-        for y in range(0, height, strip_height):
-            strip = image_array[y : y + strip_height, :]  # Get a strip
-            strips.append(strip)
-
-            # Check if any bounding box intersects with the current strip
-            label = self.check_intersection(bboxes, y, strip_height)
-            labels.append(label)
-
-        return (
-            strips,
-            labels,
-        )  # Return the list of strips and their corresponding labels
-
     def check_intersection(self, bboxes, y, strip_height):
         # Check if any bounding box intersects with the strip
         for bbox in bboxes:
