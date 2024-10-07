b962b7ef6688bb3ff57064579a2ad6034e55b633
johnathanchiu <johnathanchiu@berkeley.edu>
2024-10-07 11:38:14 -0400

    cleanup examples

--- main.py
+++ main.py
@@ -3,27 +3,26 @@ import pdfplumber
 
 from segmentor.document.segment import segment_pdf_page, segment_pdf_image
 
+pdf_file = "examples/pdfs/san-jose-pd-firearm-sample.pdf"
 
 page_idx = 0
-with pdfplumber.open(
-    "/Users/johnathanchiu/Downloads/san-jose-pd-firearm-sample.pdf"
-) as pdf:
+with pdfplumber.open(pdf_file) as pdf:
     crops = segment_pdf_page(pdf.pages[page_idx])
 
     im = pdf.pages[page_idx].to_image()
     for crop in crops:
-        im.draw_rect(crop.bbox)
+        im.draw_rect(crop.bounding_box)
 
     im.show()
 
 
-ifile = "/Users/johnathanchiu/Downloads/PM209/images/Apple_iphone-13-pro-max-07300325A-repair/images/Apple_iphone-13-pro-max-07300325A-repair_00014.jpg"
-img = Image.open(ifile).convert("RGBA")
+ifile = "examples/images/apple_iphone-13_manual.jpg"
+img = Image.open(ifile)
 
-draw = ImageDraw.Draw(img)
+draw = ImageDraw.Draw(img, "RGBA")
 for crop in segment_pdf_image(img):
     draw.rectangle(
-        crop.bounding_box, outline=(255, 0, 0), width=3, fill=(0, 127, 255, 127)
+        crop.bounding_box, outline=(255, 0, 0), width=3, fill=(0, 127, 255, 80)
     )
 
 img.show()
